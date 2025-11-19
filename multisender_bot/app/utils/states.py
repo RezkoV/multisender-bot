73334ea5f858d022beec5a542325c3029febd0f4
RezkoV <158850841+RezkoV@users.noreply.github.com>
2025-11-19 17:31:43 +0300

    Delete multisender_bot/app directory

--- multisender_bot/app/utils/states.py
+++
@@ -1,9 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-class MailingStates(StatesGroup):
-    waiting_for_content = State()
-    waiting_for_text = State()
-    waiting_for_media = State()
-    waiting_for_buttons = State()
-    waiting_for_confirmation = State()
-    waiting_for_schedule = State()
